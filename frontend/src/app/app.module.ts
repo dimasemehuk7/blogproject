ac2531bcb19395d9da4b7f9fc6ca7cea930444dc
dimasmehuk7 <dimasemehuk7@gmail.com>
2023-08-19 23:08:23 +0300

    created footer and navbar using bootstrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomePageComponent} from "./pages/home/home-page.component";
+import {NavComponent} from "./components/nav/nav.component";
+import {NgOptimizedImage} from "@angular/common";
+import {Footer} from "./components/footer/footer";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavComponent,
+    Footer
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
